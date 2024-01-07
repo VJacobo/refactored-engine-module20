c462677c8742ed9518dbc565fe895887eea0f97a
VJacobo <victoriajacobo99@gmail.com>
2024-01-06 23:14:10 -0600

    added react and Navigation and Head to Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+const Head = () => {
+    return (
+        <>
+            <header>
+                <h1>Victoria Jacobo</h1>
+                <Navigation />
+            </header>
+        </>
+    );
+};
+
+export default Head;

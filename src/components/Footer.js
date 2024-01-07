0e82f6f54fe2c70ccfbd6aa5a6044152deca8b04
VJacobo <victoriajacobo99@gmail.com>
2024-01-06 23:59:06 -0600

    added react, my github url and linkedIn URL to Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <a href="https://github.com/VJacobo" target="_blank"rel="noopener noreferrer">github</a>
+            </div>
+            <div>
+                <a href="www.linkedin.com/in/victoria-jacobo-73939b166" target="_blank" rel="noopener noreferrer">linkedin</a>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;

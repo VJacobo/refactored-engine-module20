4be7d4d2e030a5254a977b616b01189733f160e9
VJacobo <victoriajacobo99@gmail.com>
2024-01-08 19:11:12 -0600

    added resume link in Resume(); function to Resume.js

---
+++ src/pages/Resume.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Resume = () => {
+    return (
+        <section>
+            <div>
+                <a href="https://profile.indeed.com/p/victoriaj-q4ry7bl" target="_blank" rel="noopener noreferrer">Download Resume</a>
+            </div>
+            <div>
+                <p>Certified Full Stack Wed-Developer</p>
+            </div>
+        </section>
+    );
+};
+
+export default Resume;
